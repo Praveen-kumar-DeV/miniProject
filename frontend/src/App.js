b51346731012d9b328889bcfec89dd01e848d0da
Praveen-kumar-DeV <praveenkumarpatini@gmail.com>
2023-09-16 23:44:35 +0530

    integrating firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Login";
 class App extends React.Component {
   constructor(props) {
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAqY2k6c_f6HAUBzC9IvViWoDMLoAcjaSo",
+  authDomain: "praveenkumarpatini-ac74d.firebaseapp.com",
+  projectId: "praveenkumarpatini-ac74d",
+  storageBucket: "praveenkumarpatini-ac74d.appspot.com",
+  messagingSenderId: "629125667796",
+  appId: "1:629125667796:web:65d89685bfbdc01ea6d441",
+};
+const app = initializeApp(firebaseConfig);
