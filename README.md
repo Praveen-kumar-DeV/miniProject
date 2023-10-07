436bc7e70b05b6bc0e4194995c4532b795536b35
Praveen-kumar-DeV <praveenkumarpatini@gmail.com>
2023-10-07 23:16:52 +0530

    added success html filr

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # miniProject
+
 Full Stack Web Development Workshop
-- React Js is used to create frontend and Material UI is used for best User Interface.
-- Firebase is used for Sign in/Sign up with and to store the native login details in firebase.
-- Node Js is used to create a backend End point to Send mail through *Nodemailer*.
-  
+
+- A basic HTML form is presented for sending emails through the Nodemailer API.
+- Node Js is used to create a backend End point to Send mail through _Nodemailer_.
+
 # Technologies
-- ReactJs
-- Firebase
+
+- HTML5
 - NodeJs
 
 ## Available Scripts
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const path = require('path')	
+const path = require("path");
 const { transporter, mailOption } = require("./mail");
 require("dotenv").config();
 
@@ -12,7 +12,7 @@ const upload = multer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(upload.array());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //-------------end point for form body
 app.post("/sendmail", async (req, res) => {
   console.log(req.body);
@@ -26,15 +26,16 @@ app.post("/sendmail", async (req, res) => {
         res.send(err).status(400);
       } else {
         console.log("Email sent successfully");
-        res.send("sent Successfully").status(200);
+        res
+          .sendFile(path.join(__dirname, "public", "success.html"))
+          .status(200);
       }
     }
   );
 });
 
-
-app.get("/",async (req,res)=>{
-res.sendFile(path.join(__dirname,'public','index.html'));
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 app.listen(PORT, () => {
   console.log(`Server successfully started on port ${PORT}`);
